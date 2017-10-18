pixeldustproject/android_device_google_marlin

common: build kernel inline *lets build pure kernel

marlin/BoardConfig.mk
@@ -111,6 +111,13 @@ BOARD_SECCOMP_POLICY += device/google/marlin/seccomp
 
 BOARD_EGL_CFG := device/google/marlin/egl.cfg
 
+# Inline kernel building
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_SOURCE := kernel/google/marlin
+TARGET_KERNEL_CONFIG := pure_defconfig
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+
 BOARD_KERNEL_BASE        := 0x80000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x02000000

sailfish/BoardConfig.mk
@@ -107,6 +107,13 @@ BOARD_SECCOMP_POLICY += device/google/marlin/seccomp
 
 BOARD_EGL_CFG := device/google/marlin/egl.cfg
 
+# Inline kernel building
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_SOURCE := kernel/google/marlin
+TARGET_KERNEL_CONFIG := pure_defconfig
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+
 BOARD_KERNEL_BASE        := 0x80000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x02000000

Aqua-devices/device_google_marlin

Change-Id: Ie9c21bfe1145fba5a191bc6d750dedea87f5d50b

marlin/BoardConfig.mk
@@ -114,6 +114,13 @@ endif
 
 BOARD_EGL_CFG := device/google/marlin/egl.cfg
 
+# Inline kernel building
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_SOURCE := kernel/google/marlin
+TARGET_KERNEL_CONFIG := pure_defconfig
+BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
+
 BOARD_KERNEL_BASE        := 0x80000000
 BOARD_KERNEL_PAGESIZE    := 4096
 ifneq ($(filter marlin_kasan, $(TARGET_PRODUCT)),)

sailfish/BoardConfig.mk
@@ -104,6 +104,13 @@ endif
 
 BOARD_EGL_CFG := device/google/marlin/egl.cfg
 
+# Inline kernel building
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_SOURCE := kernel/google/marlin
+TARGET_KERNEL_CONFIG := pure_defconfig
+BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
+
 BOARD_KERNEL_BASE        := 0x80000000
 BOARD_KERNEL_PAGESIZE    := 4096
 ifneq ($(filter sailfish_kasan, $(TARGET_PRODUCT)),)